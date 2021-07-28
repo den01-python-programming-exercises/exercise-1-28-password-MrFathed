3a3bec975a15bee000c12b8a875d4815931daf1e
Justin Turner <jmturner638@gmail.com>
2021-07-27 21:59:19 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    password = "Caput Draconis"
+
+    guess = input("Password?")
+
+    if guess == password:
+        print("Welcome!")
+    else:
+        print("Off with you!")
 
 if __name__ == '__main__':
     main()
